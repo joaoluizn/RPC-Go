8e7b2cb5856415005c89bab4f54a925c55c32380
Vinicius Lira <vml@cin.ufpe.br>
2019-05-27 16:53:44 -0300

    commit test

---
+++ main/main/client.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/joaoluizn/RPC-go/layers/distribution/client"
+)
+
+func main() {
+	storageClient := client.NewClientProxy("0.0.0.0:8923", "Storage")
+	storageClient.Invoke("add", "")
+}
