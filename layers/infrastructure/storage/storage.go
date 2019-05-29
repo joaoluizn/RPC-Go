263f663ed6e6acedf2e717c6a2624fe5d488a83e
Vinicius Lira <vml@cin.ufpe.br>
2019-05-29 18:01:33 -0300

    commiting merge

--- layers/infrastructure/storage/storage.go
+++ layers/infrastructure/storage/storage.go
@@ -13,14 +13,17 @@ type Product struct {
 
 type Storage struct{}
 
+func NewStorage() *Storage {
+	return &Storage{}
+}
+
 const (
 	// StorageServiceName Storage Service Name to access remote procedure
 	StorageServiceName = "Storage"
 )
 
-func NewStorage() *Storage {
-	return &Storage{}
-}
+var products [4]Product
+var productsIndex = 0
 
 func PrintProductList() {
 	for i := 0; i < productsIndex; i++ {
@@ -29,8 +32,12 @@ func PrintProductList() {
 	fmt.Printf("%d\n", productsIndex)
 }
 
-var products [4]Product
-var productsIndex = 0
+func PrintCurrentItemList() {
+	for i := 0; i < productsIndex; i++ {
+		fmt.Printf("%d %s %d\n", products[i].Id, products[i].Name, products[i].Price)
+	}
+	fmt.Printf("%d\n", productsIndex)
+}
 
 func (s Storage) HelloStorage(args string, args2 string) string {
 	log.Println(args, args2)
@@ -45,7 +52,23 @@ func (s Storage) Create(name string, price int) string {
 	return "Create Function\n"
 }
 
-func (s Storage) Read() string {
+func (s Storage) AddSlice( []Product) string {
+	products[productsIndex] = Product{Id: productsIndex, Product: product}
+	productsIndex++
+	
+	PrintProductList()
+	return "Create Function\n"
+}
+
+func (s Storage) ReadList() string {
+	PrintProductList()
+
+	return "Read Function\n"
+}
+
+func (s Storage) ReadItem() string {
+	PrintProductList()
+
 	return "Read Function\n"
 }
 
